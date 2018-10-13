098e038597b5fdc7c46dc4fbb1ef8ce0c8d850ed
Cory Kim <github@cory.kim>
2018-10-13 13:36:23 -0500

    test

---
+++ index.js
@@ -0,0 +1,23 @@
+const q = require('daskeyboard-applet');
+const request = require('request-promise');
+
+const apiUrl = "https://api.weather.gov/";
+
+class WeatherAlerts extends q.DesktopApp {
+  async run() {
+    return request.post({
+      url: apiProxy,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: {
+      },
+      json: true
+    }).then(body => {
+      console.log("Got body: ", body);
+    })
+  }  
+}
+
+const applet = new WeatherAlerts();
+applet.start();
