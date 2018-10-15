6735d054cb1b38ba81dbc199c3f4e544e281ebe3
Cory Kim <github@cory.kim>
2018-10-15 15:39:01 -0500

    test

--- index.js
+++ index.js
@@ -6,33 +6,46 @@ const serviceHeaders = {
   "User-Agent": "Das Keyboard q-applet-weather"
 }
 
+var zones = null;
+
 class WeatherAlerts extends q.DesktopApp {
   async selections(fieldName) {
-    console.log("Generating selections...");
+    if (zones) {
+      return processZones(zones);
+    } else {
+
+    }
+    console.log("Retrieving zones...");
     //const zones = require('./zones.json');
     return request.get({
       url: apiUrl + '/zones',
       headers: serviceHeaders,
       json: true
     }).then(zones => {
-      //console.log("Got body: ", body);
-      const options = [];
-      for (let feature of zones.features) {
-        if (feature.properties.type === 'public') {
-          const id = feature.properties.id;
-          let label = feature.properties.name;
-          if (feature.properties.state) {
-            label = label + ', ' + feature.properties.state;
-          }
-          options.push([id, label]);
-        }
-      }
-      return options;
+      return this.processZones(zones);
     }).catch((error) => {
       console.error("Caught error:", error);
     })
   }
 
+  /**
+   * Process a zones JSON to an options list
+   * @param {*} zones 
+   */
+  async processZones(zones) {
+    const options = [];
+    for (let feature of zones.features) {
+      if (feature.properties.type === 'public') {
+        const id = feature.properties.id;
+        let label = feature.properties.name;
+        if (feature.properties.state) {
+          label = label + ', ' + feature.properties.state;
+        }
+        options.push([id, label]);
+      }
+    }
+    return options;
+  }
 
   async run() {
     console.log("Running.");
